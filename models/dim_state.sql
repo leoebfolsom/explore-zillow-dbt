9f6cc276f2ca555a9611f866f195ee6641c23be7
Leo Folsom <leoebfolsom@gmail.com>
2021-09-08 20:37:21 +0000

    reorganize models into staging and dim

--- models/dim_state.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-    STATENAME, 
-    MIN(VALUE) AS MIN_VALUE, 
-    MAX(VALUE) AS MAX_VALUE, 
-    COUNT(*) AS number_of_listings 
-FROM {{ source('zillow_share_source', 'for_sale_inventory')}}
-GROUP BY 1
---
+++ models/staging/zillow_share__public/stg_distinct_listings.sql
@@ -0,0 +1,6 @@
+WITH distinct_listings AS (
+    SELECT 
+        DISTINCT STATENAME, REGIONNAME, VALUE, MONTH
+    FROM {{ source('zillow_share_source', 'for_sale_inventory')}}
+)
+SELECT * FROM distinct_listings
