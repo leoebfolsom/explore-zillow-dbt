139d06a49c52f250f2514c0c33a888e58880d5d9
Leo Folsom <leoebfolsom@gmail.com>
2021-09-08 20:48:28 +0000

    added tests to stg_distinct_listings

--- models/staging/zillow_share__public/stg_distinct_listings.sql
+++ models/staging/zillow_share__public/stg_distinct_listings.sql
@@ -1,6 +1,9 @@
 WITH distinct_listings AS (
     SELECT 
-        DISTINCT STATENAME, REGIONNAME, VALUE, MONTH
+        DISTINCT STATENAME, 
+        REGIONNAME, 
+        VALUE, 
+        MONTH
     FROM {{ source('zillow_share_source', 'for_sale_inventory')}}
 )
 SELECT * FROM distinct_listings
